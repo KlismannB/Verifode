5f56a2a6201582dcc16f35f939548c19cad3893c
Klismann de Oliveira Barros <52436637+KlismannB@users.noreply.github.com>
2020-03-12 15:29:53 -0300

    Traducao (switch)
    Feito

---
+++ verifode.v
@@ -0,0 +1,48 @@
+
+c = i;
+i = 0;
+
+switch (c)
+	case (0):
+	// ADICIONA MAIS UM ESPAÇO NO LCD PARA SEPARAR PALAVRA.
+
+	case(1):
+		
+		if(signal == .) 
+			ENVIA = 45; //  E
+		if(signal == -)
+			ENVIA = 54; // T
+	endcase;	
+		
+	case(2):
+		if(signal == --) // ENVIA 4D - M
+		if(signal == .-) // ENVIA 41 - A
+		if(signal == ..) // ENVIA 49 - I
+		if(signal == -.) // ENVIA 4E - N
+	endcase	
+	
+	case(3):
+		if(signal == -..) //ENVIA 44 - D
+		if(signal == --.) //ENVIA 47 - G
+		if(signal == -.-) //ENVIA 4B - K
+		if(signal == ---) //ENVIA 4F - O
+		if(signal == .-.) //ENVIA 52 - R 
+		if(signal == ...) // ENVIA 53 - S
+		if(signal == ..-) // ENVIA 55 - U
+		if(signal == ..-) // ENVIA 57 - W
+		
+	case(4):
+		if(signal == -...) //ENVIA 42 - B
+		if(signal == -.-.) //ENVIA 43 - C
+		if(signal == ..-.) //ENVIA 46 - F
+		if(signal == ....) //ENVIA 48 - H
+		if(signal == .---) //ENVIA 4A - J
+		if(signal == .-..) //ENVIA 4C - L
+		if(signal == .--.) //ENVIA 50 - P
+		if(signal == --.-) //ENVIA 51 - Q
+		if(signal == ...-) //ENVIA 56 - V
+		if(signal == -..-) //ENVIA 58 - X
+		if(signal == -.--) //ENVIA 59 - Y
+		if(signal == --..) //ENVIA 5A - Z
+		
+
