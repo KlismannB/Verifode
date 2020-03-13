44a77033307d6ba9f50ad3a0bf3239179d235f3b
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2020-03-13 00:06:18 -0300

    Update morse.v

--- morse.v
+++ morse.v
@@ -1,9 +1,20 @@
 module morse (input button1, input button2, input CLKin, output reg [3:0]signal);
 integer i = 0, c;
 reg [3:0]in = 4'b0000;
-reg button_old = 1, button_raise = 0;
-
-always @(posedge CLKin) begin
+reg button_old = 1, button_raise = 0;	
+output clock_out;
+reg[27:0] counter=28'd0;
+parameter DIVISOR = 28'd27000000;	
+	
+always @(posedge CLKin)begin
+ counter <= counter + 28'd1;
+ if(counter>=(DIVISOR-1))
+  counter <= 28'd0;
+end
+	
+assign clock_out = (counter<DIVISOR/27000000)?1'b0:1'b1;
+//daria certo negedge button1 or negedge button2??
+	always @(posedge clock_out) begin
     // detect rising edge
     
     if (button_old != button2 && button2 == 1'b1)begin
